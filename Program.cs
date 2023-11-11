57efe89bc33ac83b67fb8be6aa49132a040a20ce
Maycon Padilha <maycon.jordan5000@gmail.com>
2023-11-11 12:35:18 -0300

    Sintaxe e Tipos de Dados em C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Fundamentos.Models;
+
+// Pessoa p = new Pessoa();
+
+// p.nome = "maycon";
+// p.idade = 24;
+
+// p.Apresentar();
+
+///////////////////////////////////////////////////
+
+// string apresentacao = "Ola, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.77;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
